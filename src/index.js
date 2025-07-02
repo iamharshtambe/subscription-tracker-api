a50373bf925a9634eff77b6b1e1e9cfa68d19322
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-02 19:59:30 +0530

    add routes in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import { PORT } from '../config/env.js';
+import { authRouter } from '../routes/auth.routes.js';
+import { userRouter } from '../routes/user.routes.js';
+import { subscriptionRouter } from '../routes/subscription.routes.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
    res.send('Welcome to Subscription Tracker API');
 });
+
+app.use('/api/v1/auth', authRouter);
+
+app.use('/api/v1/users', userRouter);
+
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
 app.listen(PORT, () => {
    console.log(
