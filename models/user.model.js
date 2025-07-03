e3ca7fef46767e4ec73a04b47eb87e81d6d3fc1f
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-03 14:08:50 +0530

    add user model

---
+++ models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+   {
+      name: {
+         type: String,
+         required: [true, 'User Name is required'],
+         minLength: 2,
+         maxLength: 50,
+         trim: true,
+      },
+      email: {
+         type: String,
+         required: [true, 'User Email is required'],
+         unique: true,
+         lowercase: true,
+         trim: true,
+         match: [/\S+@\S+\.\S+/, 'Please enter a valid email address'],
+      },
+      password: {
+         type: String,
+         required: [true, 'User Password is required'],
+         minLength: 6,
+      },
+   },
+   { Timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
