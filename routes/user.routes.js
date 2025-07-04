85851099f6a319b5e120e5af211614ed79899b23
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-04 21:47:08 +0530

    add auth middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { getUser, getUsers } from '../controllers/user.controller.js';
+import { authorize } from '../middlewares/auth.middleware.js';
 
 export const userRouter = Router();
 
 userRouter.get('/', getUsers);
 
-userRouter.get('/:id', getUser);
+userRouter.get('/:id', authorize, getUser);
 
 userRouter.post('/', (req, res) => {
    res.json({ title: 'CREATE new user' });
