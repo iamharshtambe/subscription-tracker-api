d0f58d8ead3bddcbdea3a6d05d148bed892f6f1a
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-04 12:37:18 +0530

    add sign-in controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -49,6 +49,41 @@ export async function signUp(req, res, next) {
    }
 }
 
-export async function signIn(req, res, next) {}
+export async function signIn(req, res, next) {
+   try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+         const error = new Error('User not found');
+         error.statusCode = 404;
+         throw error;
+      }
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+         const error = new Error('Invlaid password');
+         error.statusCode = 401;
+         throw error;
+      }
+
+      const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+         expiresIn: JWT_EXPIRES_IN,
+      });
+
+      res.status(200).json({
+         success: true,
+         message: 'User signed in successfully',
+         data: {
+            token,
+            user,
+         },
+      });
+   } catch (error) {
+      next(error);
+   }
+}
 
 export async function signOut(req, res, next) {}
